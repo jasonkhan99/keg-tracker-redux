97b581bf08215fb6b6d80283e9e5ed2dec394156
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-18 14:54:08 -0700

    use props in keglist render and changing selected keg

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -35,7 +35,7 @@ class KegControl extends React.Component {
   }
 
   handleChangingSelectedKeg = (id) => {
-    const selectedKeg = this.state.masterKegList.filter(keg => keg.id === id)[0];
+    const selectedKeg = this.props.masterKegList[id];
     this.setState({selectedKeg: selectedKeg});
   }
 
@@ -103,7 +103,7 @@ class KegControl extends React.Component {
       buttonText = "Return to Keg List";
     } else {
       currentlyVisibleState = <KegList 
-                                kegList={this.state.masterKegList} 
+                                kegList={this.props.masterKegList} 
                                 onKegSelection={this.handleChangingSelectedKeg}
                               />;
       buttonText = "Add Keg";
