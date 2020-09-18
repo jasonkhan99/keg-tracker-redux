af2a68c61d6595779a6707529d225b86c3486c4f
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-18 11:27:30 -0700

    update index.js to use redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
