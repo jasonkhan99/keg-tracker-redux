9471d870525c99e1ad8d67c1b52eebfd7e10a77f
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-18 14:55:32 -0700

    pass the props to keglist

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -7,7 +7,7 @@ function KegList(props) {
   return (
     <React.Fragment>
       <hr/>
-      {props.kegList.map((keg) =>
+      {Object.values(props.ticketList).map((ticket) =>
         <Keg
           whenKegClicked = { props.onKegSelection }
           brand={keg.brand}
@@ -24,7 +24,7 @@ function KegList(props) {
 }
 
 KegList.propTypes = {
-  kegList: PropTypes.array,
+  kegList: PropTypes.object,
   onKegSelection: PropTypes.func
 };
 
