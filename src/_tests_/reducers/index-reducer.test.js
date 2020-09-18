663ad543bb40592417e79e3fd08f8aba1ac6d252
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-18 10:04:34 -0700

    add test for default state of root reducer and pass

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -1,10 +1,15 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import kegListReducer from '../../reducers/keg-list-reducer';
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
 
   test('Should return default state if no action type is recognized', () => {
     expect(rootReducer({}, { type: null })).toEqual({
-      masterTicketList: {},
+      masterKegList: {},
       formVisibleOnPage: false
     });
   });
