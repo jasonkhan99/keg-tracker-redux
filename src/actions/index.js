21d38c8901d9965ffe33d9cb78e763212d362d1d
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-18 11:09:36 -0700

    add test for add keg action creator and pass

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,15 @@ export const deleteKeg = id => ({
 export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
 });
+
+export const addKeg = (keg) => {
+  const { brand, name, price, alcoholContent, id } = keg;
+  return {
+    type: 'ADD_KEG',
+    brand: brand,
+    name: name,
+    price: price,
+    alcoholContent: alcoholContent,
+    id: id
+  }
+}
