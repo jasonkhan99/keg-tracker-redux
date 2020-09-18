b30470550a970ae803da79c60d46d47d0c14cb73
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-18 14:47:52 -0700

    refactor to use action constants

--- src/_tests_/actions/index.test.js
+++ src/_tests_/actions/index.test.js
@@ -1,23 +1,24 @@
 import * as actions from './../../actions';
+import * as c from './../../actions/ActionTypes';
 
 describe('keg tracker actions', () => {
 
   test('should create DELETE_KEG action', () => {
     expect(actions.deleteKeg(1)).toEqual({
-      type: 'DELETE_KEG',
+      type: c.DELETE_KEG,
       id: 1
     });
   });
 
   test('should create TOGGLE_FORM action', () => {
     expect(actions.toggleForm()).toEqual({
-      type: 'TOGGLE_FORM'
+      type: c.TOGGLE_FORM
     });
   });
 
   test('should create ADD_KEG action', () => {
     expect(actions.addKeg({brand: 'Ninkasi', name: 'Red Dawn', price: '$6.00', alcoholContent: '6.7', id: 1})).toEqual({
-      type: 'ADD_KEG',
+      type: c.ADD_KEG,
       brand: 'Ninkasi',
       name: 'Red Dawn',
       price: '$6.00',
