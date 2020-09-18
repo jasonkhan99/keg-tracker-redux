c767269b307dc49d3c9deae63b52ba45658113c9
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-18 11:03:02 -0700

    add test for toggle form action creator and fail

--- src/_tests_/actions/index.test.js
+++ src/_tests_/actions/index.test.js
@@ -1,10 +1,18 @@
 import * as actions from './../../actions';
 
 describe('keg tracker actions', () => {
-  it('deleteKeg should create DELETE_KEG action', () => {
+
+  test('should create DELETE_KEG action', () => {
     expect(actions.deleteKeg(1)).toEqual({
       type: 'DELETE_KEG',
       id: 1
     });
   });
+
+  test('should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
+  
 });
