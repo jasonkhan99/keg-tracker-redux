843556ac1e499ff2401cc3629b337545b4bc7b77
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-09-18 08:59:35 -0700

    add test for default stateon no action and pass

--- src/_tests_/reducers/keg-list-reducer.test.js
+++ src/_tests_/reducers/keg-list-reducer.test.js
@@ -1,4 +1,4 @@
-import kegListReducer from '../../reducers/ticket-list-reducer';
+import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
   test('Should return default state if there is no action type passed into the reducer', () => {
